021f56ac3d34e4923bf7acee1651d08b27469b23
KaterinaGileva <Katberl@ukr.net>
2022-08-24 20:37:50 +0300

    done task-07,08 start 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,22 @@
+//Напиши скрипт, который изменяет цвета фона элемента
+  //< body > через инлайн стиль при клике
+  // на button.change - color и выводит значение 
+  //цвета в span.color.
+//Для генерации случайного цвета 
+//используй функцию getRandomHexColor.
+
+const widget = document.querySelector("widget");
+const text = document.querySelector("#text");
+
+rangeInput.addEventListener("mousemove", fontResize);
+
+function fontResize() {
+  text.style.fontSize = `${rangeInput.value}px`;
+}
+
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
