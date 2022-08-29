465632e22da8391c3aff8e45f0fc8036f41cb6ba
KaterinaGileva <Katberl@ukr.net>
2022-08-29 17:26:27 +0300

    update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -2,7 +2,6 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 const refs = {
   inputEl: document.querySelector("#controls > input"),
   createBtnEl: document.querySelector("[data-create]"),
@@ -14,15 +13,20 @@ let baseSizes = 20;
 let addValue = 10;
 
 refs.inputEl.addEventListener("input", onInputClick);
-refs.createBtnEl.addEventListener("click", createBoxes);
-refs.destroyBtnEl.addEventListener("click", onDestroyBtnElClick);
 
+//! Получаем значение amount
+function onInputClick(event) {
+  let amount = event.currentTarget.value;
+  console.log("Количество дивов -", amount);
+
+  const elements = [];
+
+  refs.createBtnEl.addEventListener("click", createBoxes);
+  refs.destroyBtnEl.addEventListener("click", onDestroyBtnElClick);
 
   //! Цикл создания div
   function createBoxes() {
     for (let i = 1; i <= amount; i += 1) {
-      const div = [];
-      amount = refs.inputEl.value;
       console.log("amount", amount);
 
       const divEl = document.createElement("div");
@@ -36,12 +40,12 @@ refs.destroyBtnEl.addEventListener("click", onDestroyBtnElClick);
     }
     refs.boxesCountainerEl.append(...elements);
   }
-
   //! Очищаем div#boxes
   function onDestroyBtnElClick() {
     refs.boxesCountainerEl.innerHTML = "";
     refs.inputEl.value = "";
+    baseSizes = 20;
 
-    console.log("div#boxes очищен!", amount);
+    console.log("div#boxes очищен!");
   }
-
+}
